c2e2fe137fde0f726d4da0b0245b3b7095fba749
mahong <1350374964@qq.com>
2016-10-01 18:25:57 +0800

    将链表和二叉树的Builder整合到相关节点类中

--- src/util/Node.java
+++ src/util/Node.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.LinkedList;
+
 public class Node {
 	public int value;
 	
@@ -12,30 +14,84 @@ public class Node {
 		return "" + value;
 	}
 	
-	public static class SingleNode extends Node {
-		public SingleNode next;
+	public static class LinkedNode extends Node {
+		public LinkedNode next;
 		
-		public SingleNode(int value) {
+		public LinkedNode(int value) {
 			super(value);
 		}
-		public static void connect(SingleNode n1, SingleNode n2) {
+		public static void connect(LinkedNode n1, LinkedNode n2) {
 			while(n1.next != null)
 				n1 = n1.next;
 			
 			n1.next = n2;
 		}
+		
+		public static class Builder {
+			public static LinkedNode convert(int... value) {
+				if (value.length == 0) return null;
+				
+				LinkedNode head = new LinkedNode(value[0]);
+				LinkedNode curr = head;
+				for (int i = 1; i < value.length; i++) {
+					LinkedNode temp = new LinkedNode(value[i]);
+					curr.next = temp;
+					curr = temp;
+				}
+				return head;
+			}
+		}
 	}
 
-	public static class DoubleNode extends Node{
-		public DoubleNode left;
-		public DoubleNode right;
-		public DoubleNode(int value) {
+	public static class BinaryNode extends Node{
+		public BinaryNode left;
+		public BinaryNode right;
+		
+		public BinaryNode(int value) {
 			super(value);
 		}
-		public DoubleNode(int value, DoubleNode left, DoubleNode right) {
+		public BinaryNode(int value, BinaryNode left, BinaryNode right) {
 			super(value);
 			this.left = left;
 			this.right = right;
+		}
+		
+		public static class Builder {
+			private BinaryNode head;
+			private BinaryNode currParent;
+			private LinkedList<BinaryNode> list;
+			public static int NULL_CHILD = Integer.MAX_VALUE;
+			public Builder(int value) {
+				head = new BinaryNode(value);
+				list = new LinkedList<>();
+				
+				list.offer(head);
+				currParent = list.poll();
+			}
+			public Builder addChild(int left, int right) {
+				BinaryNode child;
+				if (left != NULL_CHILD) {
+					child = new BinaryNode(left);
+					currParent.left = child;
+					list.offer(child);
+				}
+				else
+					currParent.left = null;
+				
+				if (right != NULL_CHILD) {
+					child = new BinaryNode(right);
+					currParent.right = child;
+					list.offer(child);
+				}
+				else
+					currParent.right = null;
+				
+				currParent = list.poll();
+				return this;
+			}
+			public BinaryNode build() {
+				return head;
+			}
 		}
 	}
 }
